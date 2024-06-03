4498d27750274177a9c70716539b434e82903020
JuanSuarez1 <juansuarez60249@gmail.com>
2024-06-03 16:18:33 -0300

    css changes

--- src/app/partials/main/MainAdmin.tsx
+++ src/app/partials/main/MainAdmin.tsx
@@ -13,18 +13,21 @@ export default function MainAdmin() {
     const [value, setValue] = React.useState<number | null>(5);
     return (
 <>
+<input className='navigation' placeholder='Buscar Producto'></input>
+<button className='buscar'>GO</button>
+<div className='admin'>Admin</div>
 <div className='fondodark'>
-<div className='switch'>Dark mode</div>
+<div>Dark mode</div>
 <Switch  onChange={darkMode} className='switch'  form="flexSwitchCheckChecked" />
    </div>
     <br></br>
       <div className='fondoimg'>
-   <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='imgg' src="./img/nisuta.webp"  /> 
+   <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='img' src="./img/nisuta.webp"  /> 
     Auriculares gamer Nisuta NSAUG300   
-    <h1>$120.000AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+    <h1>$120.000</h1>
     </a>
    
-  <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='imgg' src="./img/nisuta.webp"  /> 
+  <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='img' src="./img/nisuta.webp"  /> 
     Auriculares gamer Nisuta NSAUG300  
      <h1>$120.000</h1>
      </a>
--- src/app/partials/main/MainUser.tsx
+++ src/app/partials/main/MainUser.tsx
@@ -19,12 +19,12 @@ export default function MainUser() {
    </div>
     <br></br>
       <div className='fondoimg'>
-   <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='imgg' src="./img/nisuta.webp"  /> 
+   <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='img' src="./img/nisuta.webp"  /> 
     Auriculares gamer Nisuta NSAUG300   
     <h1>$120.000UUUUUUUUUUUUUUUUUUUUUUU</h1>
     </a>
    
-  <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='imgg' src="./img/nisuta.webp"  /> 
+  <a href="https://www.mercadolibre.com.ar/auriculares-gamer-nisuta-nsaug300-negro-y-azul/p/MLA15317091#searchVariation=MLA15317091&position=17&search_layout=stack&type=product&tracking_id=5939455b-3284-499b-9db3-18a0989a6883"  > <img className='img' src="./img/nisuta.webp"  /> 
     Auriculares gamer Nisuta NSAUG300  
      <h1>$120.000</h1>
      </a>
